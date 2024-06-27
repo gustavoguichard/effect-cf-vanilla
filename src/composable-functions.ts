b5818d0a3f933d18a6be268c9fa3210c913085f8
Guga Guichard <gustavoguichard@gmail.com>
2024-06-27 17:34:57 -0300

    Update composable-functions to 4.2.0

--- src/composable-functions.ts
+++ src/composable-functions.ts
@@ -14,10 +14,10 @@ const logger = makeLogger('Composable Functions')
 
 // Collect the data from the different sources in parallel
 const getProductPageDataBeforeDiscount = cf.collect({
-  product: cf.composable(findProductById),
-  variants: cf.composable(findVariantsByProductId),
+  product: findProductById,
+  variants: findVariantsByProductId,
   // Ensure we always return a coupon
-  coupon: cf.catchFailure(cf.composable(findCoupon), () => blankCoupon),
+  coupon: cf.catchFailure(findCoupon, () => blankCoupon),
 })
 
 // applySchema will ensure the data at runtime
@@ -34,7 +34,7 @@ const getProductPageData = cf.applySchema(paramsSchema)(
 const traceErrors = cf.trace((result) => result.errors.forEach(logger.error))
 
 // Get the arguments from the terminal and use as input to the program
-const program = traceErrors(cf.pipe(cf.composable(getArgs), getProductPageData))
+const program = traceErrors(cf.pipe(getArgs, getProductPageData))
 
 // Run the program and log the result depending on success/failure
 async function main() {
